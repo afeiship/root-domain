27d01ee71948affd3167c06eca1876c810ef68ed
afeiship <1290657123@qq.com>
2021-01-09 15:42:46 +0800

    fix: global ctx error with typeof

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,7 @@
  */
 
 (function () {
-  var global = global || window || Function('return this')();
+  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
   var nx = global.nx || require('@jswork/next');
   var nxDelete = nx.delete || require('@jswork/next-delete');
   var DEFAULT_OPTIONS = { method: 'get' };
